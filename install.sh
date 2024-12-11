317d3ab8312cbda897935b40892fcf3958407d17
aidan-gbm <aidan.mccarthy12@gmail.com>
2024-12-10 22:12:10 -0500

    fix install

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ if ! test -f "$repo/development.yml"; then
     fail cannot locate playbook
 fi
 
-ansible-playbook -K "$repo/development.yml"
+~/.local/bin/ansible-playbook -K "$repo/development.yml"
 
 if test $? -eq 0; then
     info setup complete
